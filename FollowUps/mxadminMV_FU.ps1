13e6292f242183fa48be3bc27dbb57b2e1b8a85e
bzerphey <bzerphey@telecomyork.com>
2024-01-24 15:54:44 -0500

    add mxadmin to json and create fu script

---
+++ FollowUps/mxadminMV_FU.ps1
@@ -0,0 +1,13 @@
+<#
+#Copy the needed folders for other MXAdmin versions
+#>
+
+$src = "https://file.ac/YzdYUQfTRc8/"
+$arrFolders = @("MXAdmin16.04",
+"MXAdmin17.10",
+"MXAdminShortcuts")
+
+foreach ($folder in $arrFolders) {
+    $dl = $src + $folder
+    curl.exe $dl -o "'C:\Program Files (x86)\Zultys\'$($folder)"
+}
